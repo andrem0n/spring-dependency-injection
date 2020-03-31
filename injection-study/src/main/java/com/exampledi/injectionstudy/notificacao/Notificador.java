940f3ae26ba7610ad8e9475997092317114c2e5b
André Bolzan Egert <andre.bolzan@ntconsult.com.br>
2020-03-31 16:30:17 -0300

    remoção de um dos projetos

--- injection-study/src/main/java/com/exampledi/injectionstudy/notificacao/Notificador.java
+++
@@ -1,8 +0,0 @@
-package com.exampledi.injectionstudy.notificacao;
-
-
-import com.exampledi.injectionstudy.model.Cliente;
-
-public interface Notificador {
-	void notificar(Cliente cliente, String mensagem);
-}
