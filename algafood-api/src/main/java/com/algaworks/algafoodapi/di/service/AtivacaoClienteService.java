2893dd200db18f54311916e12170b2d716c9af78
André Bolzan Egert <andre.bolzan@ntconsult.com.br>
2020-09-21 22:33:40 -0300

    auto wired

--- algafood-api/src/main/java/com/algaworks/algafoodapi/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/di/service/AtivacaoClienteService.java
@@ -2,9 +2,11 @@ package com.algaworks.algafoodapi.di.service;
 
 import com.algaworks.algafoodapi.di.modelo.Cliente;
 import com.algaworks.algafoodapi.di.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class AtivacaoClienteService {
 
+  @Autowired(required = false)
   private Notificador notificador;
 
   public AtivacaoClienteService(Notificador notificador) {
