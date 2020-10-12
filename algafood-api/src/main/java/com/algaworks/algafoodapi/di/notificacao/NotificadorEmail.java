f9a073258a697e0bf4e8d017e862a100daae6b56
André Bolzan Egert <andre.bolzan@ntconsult.com.br>
2020-10-12 19:19:56 -0300

    @Configuration properties

--- algafood-api/src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmail.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmail.java
@@ -1,6 +1,8 @@
 package com.algaworks.algafoodapi.di.notificacao;
 
 import com.algaworks.algafoodapi.di.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -12,12 +14,18 @@ public class NotificadorEmail implements Notificador {
   private boolean caixaAlta;
   private String hostServidorSmtp;
 
+  @Autowired
+  private NotificadorProperties notificadorProperties;
+
   public NotificadorEmail() {
     System.out.println("Notificador email real");
   }
 
   @Override
   public void notificar(Cliente cliente, String mensagem) {
+    System.out.println("Host: "+this.notificadorProperties.getHostServidor());
+    System.out.println("Porta: "+this.notificadorProperties.getPortaServidor());
+
     if (this.caixaAlta) {
       mensagem = mensagem.toUpperCase();
     }
